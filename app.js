42d567c74b829728cc2950df8fdea2c18dfc39d5
TRV <lstxvrt@gmail.com>
2022-05-01 08:25:44 +0530

    Add final scores pre non-case-sensitive

--- app.js
+++ app.js
@@ -12,30 +12,30 @@ function playRound(playerSelection, computerSelection) {
   if (playerSelection === computerSelection) {
     playerScore++
     computerScore++
-    console.log(`It's a tie! You both chose ${playerSelection}. Current score is ${playerScore} and ${computerScore}`);
+    console.log(`It's a tie! You both chose ${playerSelection}.`);
   } else if (playerSelection === "Rock"){
     if (computerSelection === "Paper") {
       computerScore++
-      console.log(`You lose! ${computerSelection} beats ${playerSelection}. Current score is ${playerScore} and ${computerScore}`);
+      console.log(`You lose! ${computerSelection} beats ${playerSelection}.`);
     } else if (computerSelection === "Scissors") {
       playerScore++
-      console.log(`You win! ${playerSelection} beats ${computerSelection}. Current score is ${playerScore} and ${computerScore}`);
+      console.log(`You win! ${playerSelection} beats ${computerSelection}.`);
     }
   } else if (playerSelection === "Paper") {
     if (computerSelection === "Rock") {
       playerScore++
-      console.log(`You win! ${playerSelection} beats ${computerSelection}. Current score is ${playerScore} and ${computerScore}`);
+      console.log(`You win! ${playerSelection} beats ${computerSelection}.`);
     } else if (computerSelection === "Scissors") {
       computerScore++
-      console.log(`You lose! ${computerSelection} beats ${playerSelection}. Current score is ${playerScore} and ${computerScore}`);
+      console.log(`You lose! ${computerSelection} beats ${playerSelection}.`);
     }
   } else if (playerSelection === "Scissors") {
     if (computerSelection === "Rock") {
       computerScore++
-      console.log(`You lose! ${computerSelection} beats ${playerSelection}. Current score is ${playerScore} and ${computerScore}`);
+      console.log(`You lose! ${computerSelection} beats ${playerSelection}.`);
     } else if (computerSelection === "Paper") {
       playerScore++
-      console.log(`You win! ${playerSelection} beats ${computerSelection}. Current score is ${playerScore} and ${computerScore}`);
+      console.log(`You win! ${playerSelection} beats ${computerSelection}.`);
     }
   }
 }
@@ -43,6 +43,13 @@ function playRound(playerSelection, computerSelection) {
 function game() {
   for (let i = 0; i < 5; i++) {
     playRound(prompt("Choose Rock, Paper or Scissors:"), computerPlay());
+  }
+  if (playerScore === computerScore) {
+    console.log(`The game was tied! No-one wins this game of Rock, Paper and Scissors! You scored: ${playerScore} and Computer scored ${computerScore}`);
+  } else if (playerScore > computerScore) {
+    console.log(`You win! Computer loses this game of Rock, Paper and Scissors! You scored: ${playerScore} and Computer scored ${computerScore} `);
+  } else if (computerScore > playerScore) {
+    console.log(`Computer wins! You lose this game of Rock, Paper and Scissors! You scored: ${playerScore} and Computer scored ${computerScore} `);
   }
 }
 
